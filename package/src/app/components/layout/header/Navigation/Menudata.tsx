f301dce28fc3bdb88624f043ed6d541b1e61fc91
sudarshan vr <104901361+sudarshan-vr@users.noreply.github.com>
2025-09-18 02:32:11 +0530

    Update Menudata.tsx

--- package/src/app/components/layout/header/Navigation/Menudata.tsx
+++ package/src/app/components/layout/header/Navigation/Menudata.tsx
@@ -6,6 +6,6 @@ export const headerData: HeaderItem[] = [
     { label: 'Work', href: '/#work' },
     { label: 'Team', href: '/#team' },
     { label: 'Pricing', href: '/#pricing' },
-    { label: 'Awards', href: '/#awards' },
+    { label: 'Awards & Certificates', href: '/#awards' },
     { label: 'Contact', href: '/contact' },
 ];
