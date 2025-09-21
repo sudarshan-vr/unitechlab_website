bd3938115ec1f4333e5c90f2b4877fcbae03aa06
sudarshan vr <104901361+sudarshan-vr@users.noreply.github.com>
2025-09-21 23:47:48 +0530

    Update page.tsx

--- package/src/app/page.tsx
+++ package/src/app/page.tsx
@@ -64,17 +64,6 @@ export default function Home() {
       <Solutions />
       {/* ---------------------Solutions section Ends-----------------  */}
       {/* Tawk debug initialization (optional, safe to keep) */}
-      <Script id="tawk-chat" strategy="afterInteractive">
-        {`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/68cea2bbf60acf192897a2d8/1j5jinc72';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();`}
-      </Script>
     </main>
   )
 }
