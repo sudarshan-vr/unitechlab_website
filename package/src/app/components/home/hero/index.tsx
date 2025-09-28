c7fcc5b35fa3030166276c2ba64dbc0a3cbf3188
sudarshan vr <104901361+sudarshan-vr@users.noreply.github.com>
2025-09-29 02:55:27 +0530

    Update index.tsx

--- package/src/app/components/home/hero/index.tsx
+++ package/src/app/components/home/hero/index.tsx
@@ -49,7 +49,7 @@ function HeroSection() {
               <p className='max-w-38 text-dark_black/60 dark:text-white/60'>
                 Unitechlabs is a leading technology partner for businesses of all sizes, providing high-quality, customized solutions for various industries, including healthcare, education, e-commerce, and more.
 
-With over a decade of experienced developers, we have worked with hundreds of clients, including VAPI, Navkis, Aroush pvt ltd, TOTO, Just Another IOT Company and AQResearch, to help them achieve their business goals through innovative and essential technology solutions.
+With our tech expert developers, we have worked with hundreds of clients, including VAPI, Navkis, Aroush pvt ltd, AQResearch, Just Another IoT Company and TOTO, to help them achieve their business goals through innovative and essential technology solutions.
               </p>
             </motion.div>
 
