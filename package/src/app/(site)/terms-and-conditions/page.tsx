6ab03d8415b4b7b843edeeb1d1d8ba6d7e4b4395
sudarshan vr <104901361+sudarshan-vr@users.noreply.github.com>
2025-10-05 17:59:18 +0530

    Update page.tsx

--- package/src/app/(site)/terms-and-conditions/page.tsx
+++ package/src/app/(site)/terms-and-conditions/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
                         </h1>
                         <div className="bg-white dark:bg-dark_black p-8 rounded-2xl">
                             <p>
-                                This Getnextjstemplates Terms of Service (“<span className="font-semibold">Agreement</span>”) is entered into by and between Getnextjstemplates (“<span className="font-semibold">Getnextjstemplates</span>”) and the entity or person placing an order for or accessing the Services (“<span className="font-semibold">Customer</span>”). This Agreement consists of the terms and conditions set forth below and any Order Form. The “<span className="font-semibold">Effective Date”</span> of this Agreement is the date which is the earlier of (a) Customer’s initial access to the Services through any online provisioning, registration or order process or (b) the Effective Date of the first Order Form. This Agreement will govern Customer’s initial purchase on the Effective Date as well as any future purchases made by Customer that reference this Agreement. Getnextjstemplates may modify this Agreement from time to time as permitted in Section 13.4 (Amendment).
+                                This Unitechlabs Terms of Service (“<span className="font-semibold">Agreement</span>”) is entered into by and between Unitechlabs (“<span className="font-semibold">Unitechlabs</span>”) and the entity or person placing an order for or accessing the Services (“<span className="font-semibold">Customer</span>”). This Agreement consists of the terms and conditions set forth below and any Order Form. The “<span className="font-semibold">Effective Date”</span> of this Agreement is the date which is the earlier of (a) Customer’s initial access to the Services through any online provisioning, registration or order process or (b) the Effective Date of the first Order Form. This Agreement will govern Customer’s initial purchase on the Effective Date as well as any future purchases made by Customer that reference this Agreement. Unitechlabs may modify this Agreement from time to time as permitted in Section 13.4 (Amendment).
                             </p>
                             <p className="mt-5">
                                 Capitalized terms shall have the meanings set forth in Section 1, or in the section where they are first used
@@ -38,12 +38,12 @@ export default function Page() {
                                     </li>
                                     <li className="mt-5">
                                         <p><span className="font-semibold">1.3 “Documentation”</span>
-                                            means the technical materials made available by Getnextjstemplates to Customer and/or its Users in hard copy or electronic form describing the use and operation of the Services.</p>
+                                            means the technical materials made available by Unitechlabs to Customer and/or its Users in hard copy or electronic form describing the use and operation of the Services.</p>
                                     </li>
                                     <li className="mt-5">
                                         <p>
                                             <span className="font-semibold">1.4 “Services”</span>
-                                            Getnextjstemplates’s proprietary web-based products and services, along with downloadable desktop and mobile apps. Each Order Form will identify details of Customer’s Services subscription.
+                                            Unitechlabs’s proprietary web-based products and services, along with downloadable desktop and mobile apps. Each Order Form will identify details of Customer’s Services subscription.
                                         </p>
                                     </li>
                                     <li className="mt-5">
@@ -51,7 +51,7 @@ export default function Page() {
                                             <span className="font-semibold">
                                                 1.5 “Order Form”
                                             </span>
-                                            means a document signed by both Parties identifying the Enterprise Services to be made available by Getnextjstemplates pursuant to this Agreement.
+                                            means a document signed by both Parties identifying the Enterprise Services to be made available by Unitechlabs pursuant to this Agreement.
                                         </p>
                                     </li>
                                     <li className="mt-5">
@@ -61,7 +61,7 @@ export default function Page() {
                                             <span className="font-semibold">
                                                 “Components”
                                             </span>
-                                            means add-on modules made available within the Services. Packages and Components may be created by Getnextjstemplates, Customer or other third parties. Packages and Components created by Getnextjstemplates are supported as part of the Services. Getnextjstemplates will use reasonable efforts to support Customer’s use of Packages and Components created by third parties but disclaims all warranties as to such Packages and Components.
+                                            means add-on modules made available within the Services. Packages and Components may be created by Unitechlabs, Customer or other third parties. Packages and Components created by Unitechlabs are supported as part of the Services. Unitechlabs will use reasonable efforts to support Customer’s use of Packages and Components created by third parties but disclaims all warranties as to such Packages and Components.
                                         </p>
                                     </li>
                                     <li className="mt-5">
@@ -75,7 +75,13 @@ export default function Page() {
                                     <li className="mt-5">
                                         <p>
                                             <span className="font-semibold">1.8 “Services”</span>
-                                            means Getnextjstemplates’s SaaS product, web design software, tools, along with downloadable desktop and mobile apps. Each Order Form will identify details of Customer’s subscription to the Services.
+                                            means Unitechlabs’s SaaS product, web design software, tools, along with downloadable desktop and mobile apps. Each Order Form will identify details of Customer’s subscription to the Services.
+                                        </p>
+                                    </li>
+                                    <li className="mt-5">
+                                        <p>
+                                            <span className="font-semibold">1.9 “Lead Data Terms”</span>
+                                            All lead data provided by UnitechLabs is strictly confidential and intended solely for the client’s business use. Sharing, reselling, or misusing this information in any form may result in termination of services and potential legal action.
                                         </p>
                                     </li>
                                 </ul>
@@ -86,13 +92,13 @@ export default function Page() {
                                     <li>
                                         <p>
                                             <span className="font-semibold">2.1 Services</span>{" "}
-                                            Getnextjstemplates hereby grants Customer a non-exclusive, non-transferable license during the Term (as defined in Section 12) to: (a) use the Services and to download and install desktop or mobile applications as applicable on the number and type of Authorized Devices solely for Customer’s internal business purposes in accordance with the Documentation, and/or (b) use our SaaS product, hosted systems, design software, tools, and build websites under the Getnextjstemplates.app domain.. The Services are delivered electronically.
+                                            Unitechlabs hereby grants Customer a non-exclusive, non-transferable license during the Term (as defined in Section 12) to: (a) use the Services and to download and install desktop or mobile applications as applicable on the number and type of Authorized Devices solely for Customer’s internal business purposes in accordance with the Documentation, and/or (b) use our SaaS product, hosted systems, design software, tools, and build websites under the Unitechlabs.app domain.. The Services are delivered electronically.
                                         </p>
                                     </li>
                                     <li className="mt-5">
                                         <p>
                                             <span className="font-semibold">2.2 Provisioning the Services</span>{" "}
-                                            Getnextjstemplates will provide to Customer the necessary passwords, security protocols, policies, network links or connections (“Access Protocols”) to allow Customer and its Users to access the Services as described herein; no other access to the website or servers from which the Services are delivered is permitted. Customer will provision its Users  to access and use the features and functions of the Services through the Access Protocols. Customer may select one or more Users to act as administrators and control, manage and use the Services on Customer’s behalf. Customer shall be responsible for all acts and omissions of its Users
+                                            Unitechlabs will provide to Customer the necessary passwords, security protocols, policies, network links or connections (“Access Protocols”) to allow Customer and its Users to access the Services as described herein; no other access to the website or servers from which the Services are delivered is permitted. Customer will provision its Users  to access and use the features and functions of the Services through the Access Protocols. Customer may select one or more Users to act as administrators and control, manage and use the Services on Customer’s behalf. Customer shall be responsible for all acts and omissions of its Users
                                         </p>
                                     </li>
                                 </ul>
